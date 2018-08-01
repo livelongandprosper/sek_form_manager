54aed896847498867290d3198df98d4b69722040
Gregor Wendland <gregor@gewend.de>
2018-08-01 08:49:31 +0200

    inital commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title></title>
+
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+		<meta name="viewport" content="width=device-width">
+	</head>
+	<body>
+
+	</body>
+</html>
