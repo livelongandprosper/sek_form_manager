e45fde6a94eede816528ec9cda913bbdfceca22a
Gregor Wendland <gregor@gewend.de>
2018-08-03 09:20:20 +0200

    added config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,16 @@
+<?php
+	// DB login data
+	if(!defined("DB_HOST")) {
+		define("DB_HOST", "localhost");
+	}
+	if(!defined("DB_NAME")) {
+		define("DB_NAME", "sek_form_manager");
+	}
+	if(!defined("DB_USER")) {
+		define("DB_USER", "root");
+	}
+	if(!defined("DB_PASSWORD")) {
+		define("DB_PASSWORD", "");
+	}
+
+?>
